16369107eeb3db4c936ddaf9fc1cd801d6dbe1d7
faizanwebstorms <faizanibrahimtws@gmail.com>
2024-05-28 19:57:42 +0500

    index js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ mongoose.set('strictQuery', true);
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
   server = app.listen(config.port, () => {
-    logger.info(`Listening to port ${config.port}`);
+    logger.info(`Listening to port Hellow Workd`);
+    console.log("sddddaaaaaaaaaaaaaaaaaaaaaaaaaa")
   });
 });
 
