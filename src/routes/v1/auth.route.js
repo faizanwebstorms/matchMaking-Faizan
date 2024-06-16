ee2c27b1e7a04079a84ccac6f07726d8d1b849aa
faizanwebstorms <faizanibrahimtws@gmail.com>
2024-06-16 16:28:37 +0500

    register validationn

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -7,7 +7,7 @@ const auth = require('../../middlewares/auth');
 const router = express.Router();
 
 // router.post('/register', validate(authValidation.register), authController.register);
-router.post('/register', authController.register);
+router.post('/register', validate(authValidation.register), authController.register);
 router.post('/login/social', validate(authValidation.loginSocial), authController.loginSocial);
 router.post('/login', validate(authValidation.login), authController.login);
 router.post('/logout', validate(authValidation.logout), authController.logout);
