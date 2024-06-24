7c1546dc851a59c732995cd36d052355b4c306b3
faizanwebstorms <faizanibrahimtws@gmail.com>
2024-06-24 22:10:04 +0500

    Socket emit function

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,22 +1,29 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const socketIO = require('socket.io');
-
+const socketIO = require("socket.io");
 
 const users = [];
 const socketConnection = (server) => {
   const io = socketIO(server, {
     cors: {
-      origin: '*',
+      origin: "*",
     },
   });
-  io.on('connection', async (socket) => {
-    socket.on('disconnect', async () => {
-      console.log('User disconnected');
+  io.on("connection", async (socket) => {
+    // join new room
+    socket.on("join-room", async () => {
+      console.log(" User Joined Room Successfully ");
+      return socket.emit("error", {
+        error: "roomId and userId will not be epmty",
+      });
+    });
+
+    socket.on("disconnect", async () => {
+      console.log("User disconnected");
+      return socket.emit("User disconnected");
     });
   });
   return io;
 };
-
 
 module.exports = {
   socketConnection,
