d6b6f3370ef5e12b0d09f387ab3bb826908ebe3c
faizanwebstorms <faizanibrahimtws@gmail.com>
2024-06-25 19:20:38 +0500

    create messages and get room messages functions

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,7 @@
-module.exports.authService = require('./auth.service');
-module.exports.tokenService = require('./token.service');
-module.exports.userService = require('./user.service');
-module.exports.reactionService = require('./reaction.service');
-module.exports.emailService = require('./email.service');
-module.exports.otpService = require('./otp.service')
+module.exports.authService = require("./auth.service");
+module.exports.tokenService = require("./token.service");
+module.exports.userService = require("./user.service");
+module.exports.reactionService = require("./reaction.service");
+module.exports.emailService = require("./email.service");
+module.exports.otpService = require("./otp.service");
+module.exports.chatService = require("./chat.service");
