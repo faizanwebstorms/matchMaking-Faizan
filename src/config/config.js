3ff15b3e997dff64d2b1e1dac1a89aef9eaa6d19
faizanwebstorms <faizanibrahimtws@gmail.com>
2024-08-24 13:32:24 +0500

    swagger testing

--- src/config/config.js
+++ src/config/config.js
@@ -1,39 +1,51 @@
-const dotenv = require('dotenv');
-const path = require('path');
-const Joi = require('joi');
+const dotenv = require("dotenv");
+const path = require("path");
+const Joi = require("joi");
 
-dotenv.config({ path: path.join(__dirname, '../../.env') });
+dotenv.config({ path: path.join(__dirname, "../../.env") });
 
 const envVarsSchema = Joi.object()
   .keys({
-    NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
+    NODE_ENV: Joi.string()
+      .valid("production", "development", "test")
+      .required(),
     PORT: Joi.number().default(3000),
-    HOST: Joi.string().default('localhost'),
-    MONGODB_URL: Joi.string().required().description('Mongo DB url'),
-    JWT_SECRET: Joi.string().required().description('JWT secret key'),
-    JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
-    JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
-    JWT_SOCIAL_EXPIRATION_DAYS: Joi.number().default(365).description('days after which Social tokens expire'),
+    HOST: Joi.string().default("3.70.233.161"),
+    MONGODB_URL: Joi.string().required().description("Mongo DB url"),
+    JWT_SECRET: Joi.string().required().description("JWT secret key"),
+    JWT_ACCESS_EXPIRATION_MINUTES: Joi.number()
+      .default(30)
+      .description("minutes after which access tokens expire"),
+    JWT_REFRESH_EXPIRATION_DAYS: Joi.number()
+      .default(30)
+      .description("days after which refresh tokens expire"),
+    JWT_SOCIAL_EXPIRATION_DAYS: Joi.number()
+      .default(365)
+      .description("days after which Social tokens expire"),
     JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
       .default(10)
-      .description('minutes after which reset password token expires'),
+      .description("minutes after which reset password token expires"),
     JWT_PHONE_VERIFICATION_EXPIRATION_MINUTES: Joi.number()
       .default(10)
-      .description('minutes after which reset password token expires'),
+      .description("minutes after which reset password token expires"),
     JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number()
       .default(10)
-      .description('minutes after which verify email token expires'),
-    SMTP_HOST: Joi.string().description('server that will send the emails'),
-    SMTP_PORT: Joi.number().description('port to connect to the email server'),
-    SMTP_USERNAME: Joi.string().description('username for email server'),
-    SMTP_PASSWORD: Joi.string().description('password for email server'),
-    EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
+      .description("minutes after which verify email token expires"),
+    SMTP_HOST: Joi.string().description("server that will send the emails"),
+    SMTP_PORT: Joi.number().description("port to connect to the email server"),
+    SMTP_USERNAME: Joi.string().description("username for email server"),
+    SMTP_PASSWORD: Joi.string().description("password for email server"),
+    EMAIL_FROM: Joi.string().description(
+      "the from field in the emails sent by the app"
+    ),
     VIN_TOKEN: Joi.string(),
     GOOGLE_API_KEY: Joi.string(),
   })
   .unknown();
 
-const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+const { value: envVars, error } = envVarsSchema
+  .prefs({ errors: { label: "key" } })
+  .validate(process.env);
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -44,7 +56,7 @@ module.exports = {
   port: envVars.PORT,
   host: envVars.HOST,
   mongoose: {
-    url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
+    url: envVars.MONGODB_URL + (envVars.NODE_ENV === "test" ? "-test" : ""),
     options: {},
   },
   jwt: {
@@ -52,8 +64,10 @@ module.exports = {
     accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
     refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
     socialExpirationDays: envVars.JWS_SOCIAL_EXPIRATION_DAYS,
-    resetPasswordExpirationMinutes: envVars.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
-    verifyPhoneExpirationMinutes: envVars.JWT_PHONE_VERIFICATION_EXPIRATION_MINUTES,
+    resetPasswordExpirationMinutes:
+      envVars.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
+    verifyPhoneExpirationMinutes:
+      envVars.JWT_PHONE_VERIFICATION_EXPIRATION_MINUTES,
     verifyEmailExpirationMinutes: envVars.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES,
   },
   email: {
