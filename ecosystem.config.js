5e25e43763883240638b7096b489344a8ed272e6
faizanwebstorms <faizanibrahimtws@gmail.com>
2024-08-25 23:16:20 +0500

    small configurastion changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,16 +5,16 @@ module.exports = {
       script: "./src/app.js", // Update this if your entry point is different
       watch: true, // Enable watch mode to restart on changes
       ignore_watch: ["node_modules"], // Ignore node_modules to prevent unnecessary restarts
-      env: {
-        NODE_ENV: "development",
-        PORT: 3000,
-        // Add any other environment variables here
-      },
-      // env_production: {
-      //   NODE_ENV: 'production',
+      // env: {
+      //   NODE_ENV: "development",
       //   PORT: 3000,
-      //   // Add any production-specific environment variables here
-      // }
+      //   // Add any other environment variables here
+      // },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3000,
+        // Add any production-specific environment variables here
+      },
     },
   ],
 };
